54d29c06cb140546246511b299a37d824230f59f
Kat Type M Productions <83372001+KatTypeM@users.noreply.github.com>
2025-05-22 15:45:14 -0400

    1.5.0
    update branch created for v1.5.0

---
+++ CHANGELOG.md
@@ -0,0 +1,127 @@
+
+# Change Log
+All notable changes to this project will be documented in this file.
+ 
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+
+## [1.5.0] - 2025-05-22
+
+### Added
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  CHANGELOG.md file, moved revision list from manifest.json
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Duplicate item checking logic for input field and active tab
+  
+### Changed
+ 
+### Fixed
+
+
+## [1.4.1] - 2025-05-04
+ 
+### Added
+
+### Changed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Rearrange button style updated for sizing error
+  
+### Fixed
+
+
+## [1.4.0] - 2025-05-03
+
+### Added
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Added options button
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Added rearrange list items button to option sections
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Added rearrange buttons to each list item so they can move items on the list
+ 
+### Changed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  moved clipboard button to options section
+  
+### Fixed
+
+
+## [1.3.0] - 2025-03-04
+ 
+### Added
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Added windows clipboard copy
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Added hover effects to buttons
+ 
+### Changed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  updated references to objects
+  
+### Fixed
+
+
+## [1.2.0] - 2024-11-27
+ 
+added icon support 
+ 
+### Added
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  added icon support with different sizes
+
+### Changed
+ 
+### Fixed
+ 
+ 
+## [1.1.0] - 
+ 
+### Added
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Clear list button toggle event, 
+
+### Changed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  visual and spacing updates
+ 
+### Fixed
+
+
+## [1.0.2] - 2023-01-14
+ 
+Sizing update
+ 
+### Added
+
+### Changed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  Sizing update
+  
+### Fixed
+ 
+ 
+## [1.0.1] - 
+
+visual updates
+
+### Added
+ 
+### Changed
+ 
+### Fixed
+- [Review URL Later](https://github.com/KatTypeM/chrome-extension)
+  visual updates
+ 
+ 
+## [1.0.0] - 
+
+Initial release
+ 
+### Added
+   
+### Changed
+ 
+### Fixed
+ 
+
--- js.js
+++ js.js
@@ -4,14 +4,20 @@
 
 //Chrome Extension Review URL Later
 const inputEl = document.querySelector("#input-el");
-const inputBtn = document.querySelector("#input-btn");
-const tabBtn = document.querySelector("#tab-btn");
-const deleteBtn = document.querySelector("#delete-btn");
+const btnInput = document.querySelector("#btn-input");
+const btnTab = document.querySelector("#btn-tab");
+const btnDelete = document.querySelector("#btn-delete");
+const btnClipboard = document.querySelector("#btn-clipboard");
 const messageEl = document.querySelector("#message-el");
 const savedUlEl = document.querySelector("#savedul-el");
-const yesBtn = document.querySelector("#yes-btn");
-const noBtn = document.querySelector("#no-btn");
+const btnYes = document.querySelector("#btn-yes");
+const btnNo = document.querySelector("#btn-no");
+const btnOptions = document.querySelector("#btn-options")
+const containerOptions = document.querySelector("#container-options")
+const btnRearrangeItems = document.querySelector("#btn-rearrange-items")
 let savedUrl = [];
+let clearlistCount = 0;
+let rearrangeItemsBool = false;
 
 //Get information from localStorage
 if (localStorage.getItem("savedItems") === null){
@@ -21,17 +27,24 @@ if (localStorage.getItem("savedItems") === null){
 	renderList(savedUrl);
 }
 //
-inputBtn.addEventListener("click", function() {
+btnInput.addEventListener("click", function() {
 	//check value in box, push to array
 	if (inputEl.value === ""){
 		messageEl.textContent = "Please enter information into the box";
 	} else {
 		messageEl.textContent = "Your list of saved items:";
-		savedUrl.push(inputEl.value);
+		// check for duplicates
+		if (checkForDuplicateInput(inputEl.value)) { 
+			messageEl.textContent = "Duplicate item not added to list.";
+		} else {
+			messageEl.textContent = "Your list of saved items:";
+			savedUrl.push(inputEl.value)
+		};
 	};
-	yesBtn.style.display = "none";
-	noBtn.style.display = "none";
+	btnYes.style.display = "none";
+	btnNo.style.display = "none";
 	inputEl.value = "";
+	containerOptions.classList.add("hidden")
 	renderList(savedUrl);
 })
 //render information to extension 
@@ -40,7 +53,18 @@ function renderList(saved){
 	let listItems = ""; 
 	//rebuild list
 	for (let i = 0; i < saved.length; i++){
-		listItems += `<li><a href="${saved[i]}" target="_blank">${saved[i]}</a>   <button id="${i}" class="remove-btn">DELETE</button></li>`;
+		listItems += `
+		<li>
+		<a href="${saved[i]}" target="_blank">${saved[i]}</a>
+		<div class="container-li-btn">
+			<div class="container-btn-up-down">
+				<button class="btn-color btn-up-down hidden" id="btn-up-${i}">&#9206</button>
+				<button class="btn-color btn-up-down hidden" id="btn-down-${i}">&#9207</button>
+			</div>
+			<button class="btn-color-white remove-btn" id="delete-${i}">DELETE</button>
+		</div>
+		</li>
+		`;
 	}
 	//send list to page
 	savedUlEl.innerHTML = listItems;
@@ -48,48 +72,174 @@ function renderList(saved){
 }
 //event listener for deleting individual entries 
 document.addEventListener("click",
-(e) => {
-	let element = e.target;
-	if (element.tagName == "BUTTON" && element.textContent === "DELETE") {
-		spliceSavedUrl(element.id);
-	}
-}, false
+	(e) => {
+		let element = e.target;
+		let tempId = e.target.id.split("-");
+		let elementContent = element.textContent;
+		
+		// convert
+		if (element.textContent === "⏶"){ elementContent = "&#9206"};
+		if (element.textContent === "⏷"){ elementContent = "&#9207"};
+		
+		// delete list item
+		if (element.tagName === "BUTTON" && element.textContent === "DELETE") {
+			deleteSavedUrlListItem(tempId[1]);
+		}
+		// move list item
+		if (element.tagName === "BUTTON" && (elementContent === "&#9206" || elementContent === "&#9207" )){
+			reorderSavedUrlList(tempId[1], tempId[2]);
+		}
+		
+	}, false
 )
 // splice individual entry from savedUrl array
-function spliceSavedUrl(inputid){
-	let deleted = savedUrl.splice(inputid,1)
+function deleteSavedUrlListItem(inputId){
+	let deleted = savedUrl.splice(inputId,1);
 	renderList(savedUrl);
+	displayRearrangeButtons();
 }
+// move items up or down in priority in the list
+function reorderSavedUrlList(direction, fromIndex){
+	let toIndex = parseInt(fromIndex);
+	
+	if(direction === "up"){
+		toIndex -= 1;
+	}
+	if(direction === "down"){
+		toIndex += 1;
+	}
+
+	if(toIndex < 0 || toIndex > savedUrl.length ){
+		return
+	} else {
+		let itemRemoved = savedUrl.splice(fromIndex, 1)
+		savedUrl.splice( toIndex, 0, itemRemoved)
+	}
+	
+	renderList(savedUrl);
+	displayRearrangeButtons();
+}
+
+// check for duplicate inputs and urls
+function checkForDuplicateInput(_input) {
+	let result;
+	_input.toString()
+	savedUrl.find((e) => {
+		if ( e === _input) {
+			result = true;
+			return true;
+		}
+		result = false
+		return false;
+	})
+	return result
+}
+
 //Clear list button and submenu
-deleteBtn.addEventListener("click", function(){
-	messageEl.textContent = "Are you sure you want to clear the list?";
-	yesBtn.style.display = "inline";
-	noBtn.style.display = "inline";
+btnDelete.addEventListener("click", function(event){
+	if(clearlistCount == 0){
+		messageEl.textContent = "Are you sure you want to clear the list?";
+		btnYes.style.display = "inline";
+		btnNo.style.display = "inline";
+		clearlistCount = 1;
+	} else {
+		
+		messageEl.textContent = "Your list of saved items:";
+		btnYes.style.display = "none";
+		btnNo.style.display = "none";
+		clearlistCount = 0;
+	}
 })
 // clear list yes to clearing
-yesBtn.addEventListener("click",  function clearList(){
+btnYes.addEventListener("click",  function clearList(){
 	messageEl.innerHTML = "Please enter information into the box";
 	for (let i = savedUrl.length; i > -1; i--){
 		savedUrl.pop();
 	}
 	savedUlEl.innerHTML = "";
 	localStorage.removeItem("savedItems");
-	yesBtn.style.display = "none";
-	noBtn.style.display = "none";
+	btnYes.style.display = "none";
+	btnNo.style.display = "none";
+	clearlistCount = 0;
+	rearrangeItemsBool = false;
+	containerOptions.classList.add("hidden")
 })
 // clear list no to clearing
-noBtn.addEventListener("click",  function backToList(){
+btnNo.addEventListener("click",  function backToList(){
 	messageEl.textContent = "Your list of saved items:";
-	yesBtn.style.display = "none";
-	noBtn.style.display = "none";
+	btnYes.style.display = "none";
+	btnNo.style.display = "none";
+	clearlistCount = 0;
 })
 // store current tab to list
-tabBtn.addEventListener("click", function(){
+btnTab.addEventListener("click", function(){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-		savedUrl.push(tabs[0].url);
+		// check for duplicates
+		if (checkForDuplicateInput(tabs[0].url)) { 
+			messageEl.textContent = "Duplicate item not added to list.";
+		} else {
+			messageEl.textContent = "Your list of saved items:";
+			savedUrl.push(tabs[0].url)
+		};
 		inputEl.value = "";
 		renderList(savedUrl);
 	});
-	yesBtn.style.display = "none";
-	noBtn.style.display = "none";
+	btnYes.style.display = "none";
+	btnNo.style.display = "none";
+	clearlistCount = 0;
+	containerOptions.classList.add("hidden")
+})
+
+
+// toggles the additional options row
+btnOptions.addEventListener("click", () => {
+
+	if(containerOptions.classList.contains("hidden")){
+		containerOptions.classList.remove("hidden")
+		
+	} else {
+		containerOptions.classList.add("hidden")
+	}
+	rearrangeItemsBool = false;
+	displayRearrangeButtons();
+})
+
+// toggles the arrange items buttons on / off
+btnRearrangeItems.addEventListener("click", () => {
+	if(rearrangeItemsBool === false){
+		rearrangeItemsBool = true;
+		
+	} else {
+		rearrangeItemsBool = false;
+	}
+	displayRearrangeButtons();
+})
+function displayRearrangeButtons(){
+	let liElement = document.querySelectorAll('li a');
+	console.log(liElement)
+	if (rearrangeItemsBool){
+		messageEl.textContent = "Rearrange the items in the list:";
+		liElement.forEach((e) => e.style.maxWidth = "265px");
+	} else {
+		messageEl.textContent = "Your list of saved items:";
+		liElement.forEach((e) => e.style.maxWidth = "295px");
+	}
+	
+	
+	let itemList = document.querySelectorAll(`[class~="btn-up-down"]`)
+	itemList.forEach((e) => {
+		if(rearrangeItemsBool){
+			e.classList.remove("hidden")
+		}
+		if(!rearrangeItemsBool){
+			e.classList.add("hidden")
+		}
+	})
+}
+
+// copies entire list to windows clipboard
+btnClipboard.addEventListener("click", function copytoclipboard(){
+	let urlToCopy = savedUrl.join("\n");
+	navigator.clipboard.writeText(urlToCopy);
+	containerOptions.classList.add("hidden");
 })
